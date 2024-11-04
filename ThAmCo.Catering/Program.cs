2455baa3115c481f71e0f1c477fca7e798972e8b
alisavari <80645653+alisavari@users.noreply.github.com>
2024-11-04 23:49:19 +0000

    Controllers Added

--- ThAmCo.Catering/Program.cs
+++ ThAmCo.Catering/Program.cs
@@ -1,3 +1,6 @@
+using ThAmCo.Catering.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Register database context with the framework
+builder.Services.AddDbContext<CateringDbContext>();
 
 
 
